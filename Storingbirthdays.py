12d5171cd4509fcab50842084c303a3b665e1202
Anirudhcreate <10016@sanskritischool.edu.in>
2025-10-04 14:53:40 +0530

    initial commit

---
+++ Storingbirthdays.py
@@ -0,0 +1,7 @@
+a=13
+b=10
+c=25
+d=3
+e=27
+print("Upcoming Birthdays!")
+print("\n" ,a, "February,", "then,",b, "April,",  "then,",c, "September,",  "then,",d ,"December,",  "then,",e, "December,")
